18ad2c1ed7ddf869ee003c8d8047203946a8d998
MourichidatouBIOKOU <mourbiokou8@gmail.com>
2025-11-30 13:08:49 +0100

    Add files via upload

---
+++ model/assignment.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+// Gestion de la pagination
+var aggregatePaginate = require("mongoose-aggregate-paginate-v2");
+
+
+let AssignmentSchema = Schema({
+    id: Number,
+    dateDeRendu: Date,
+    nom: String,
+    rendu: Boolean
+});
+
+AssignmentSchema.plugin(aggregatePaginate);
+
+// C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
+module.exports = mongoose.model('Assignment', AssignmentSchema);
